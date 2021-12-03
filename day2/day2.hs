02a28c9813027b4fc68e005354458adf126979ed
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2021-12-03 09:04:54 +0100

    simplify

--- day2/day2.hs
+++ day2/day2.hs
@@ -12,15 +12,13 @@ readInt :: String -> Int
 readInt = read
 
 sol :: [Move] -> Int
-sol moves = sum (map (getMovement Vertical) moves) * sum (map (getMovement Horizontal) (filter isForward moves))
+sol moves = sum (map (getMovement Vertical) moves) * sum (map (getMovement Horizontal) moves)
 
 sol2 :: [Move] -> Int
 sol2 moves =  sum [aim * forward | (aim, forward) <- zip (aims moves []) (map (getMovement Horizontal) moves)] * sum (map (getMovement Horizontal) moves)
 
 aims :: [Move] -> [Int] -> [Int]
-aims [] as = as
-aims (m:ms) [] = aims ms [getMovement Vertical m]
-aims (m:ms) as = aims ms (as ++ [last as + getMovement Vertical m])
+aims ms as = tail (foldl (\as m -> as ++ [last as + getMovement Vertical m]) [0] ms)
 
 data Move = Up Int | Down Int | Forward Int deriving (Show, Read)
 data Direction = Horizontal | Vertical deriving (Show)
@@ -30,15 +28,6 @@ getMovement Vertical (Up m) = -m
 getMovement Vertical (Down m) = m
 getMovement Horizontal (Forward m) = m
 getMovement _ _ = 0
-
-isUp (Up _) = True
-isUp _      = False
-
-isDown (Down _) = True
-isDown _        = False
-
-isForward (Forward _) = True
-isForward _           = False
 
 capitalize :: String -> String
 capitalize (head:tail) = toUpper head : tail
